6ce2dc4240c1704abcb3a1f591a90a520d1914c9
RayWang <759039391@qq.com>
2015-09-24 14:30:03 +0800

    可以实现View绑定，OnClick事件绑定 ，以及使用基本的settext绑定值的数据绑定（仅限）Adapter

--- src/wang/raye/preioc/PreIOC.java
+++ src/wang/raye/preioc/PreIOC.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.util.Log;
 import android.view.View;
+import android.widget.BaseAdapter;
 import wang.raye.preioc.find.AbstractFind;
 import wang.raye.preioc.find.ActivityFind;
 import wang.raye.preioc.find.DialogFind;
@@ -31,7 +32,7 @@ public class PreIOC {
 	};
 	/** 缓存*/
 	private static final Map<Class<?>, ViewBinder<Object>> BINDERS = new LinkedHashMap<>();
-	
+	private static final Map<Class<?>, ViewDataBinder> DATABINDERS = new LinkedHashMap<>();
 
 	public static void binder(Activity activity) {
 		binder(activity,activity,new ActivityFind());
@@ -63,6 +64,21 @@ public class PreIOC {
 		binder(target,dialog,new DialogFind());
 	}
 	
+	/**
+	 * 绑定数据
+	 * @param viewHolder
+	 */
+	public static void binderData(Object viewHolder,BaseAdapter adapter,int position){
+		Class<?> targetClass = viewHolder.getClass();
+		try {
+			ViewDataBinder vdb = findVDBForClass(targetClass);
+			if(vdb != null){
+				vdb.bindData(viewHolder, adapter, position);
+			}
+		}catch(Exception e){
+			throw new RuntimeException("Unable to bind data for " + targetClass.getName(), e);
+		}
+	}
 	/**
 	 * 绑定指定类对象的属性
 	 * @param target 被绑定的属性的所在类的对象
@@ -111,6 +127,35 @@ public class PreIOC {
 			viewBinder = findViewBinderForClass(cls.getSuperclass());
 		}
 		BINDERS.put(cls, viewBinder);
+		return viewBinder;
+	}
+	
+	private static ViewDataBinder findVDBForClass(Class<?> cls)
+			throws IllegalAccessException, InstantiationException {
+		ViewDataBinder viewBinder = DATABINDERS.get(cls);
+		if (viewBinder != null) {
+			if (debug)
+				Log.d(TAG, "ProIOC: 从缓存中获取到.");
+			return viewBinder;
+		}
+		
+		String clsName = cls.getName();
+		if (clsName.startsWith("android.") || clsName.startsWith("java.")) {
+			if (debug)
+				Log.w(TAG, "ProIOC: 当前类不支持注入");
+			return null;
+		}
+		try {
+			Class<?> viewBindingClass = Class.forName(clsName + "$$ViewDataBinder");
+			viewBinder = (ViewDataBinder) viewBindingClass.newInstance();
+			if (debug)
+				Log.d(TAG, "ProIOC: 成功加载ViewBinder.");
+		} catch (ClassNotFoundException e) {
+			if (debug)
+				Log.w(TAG, "实例化失败，重新搜寻 " + cls.getSuperclass().getName());
+			viewBinder = findVDBForClass(cls.getSuperclass());
+		}
+		DATABINDERS.put(cls, viewBinder);
 		return viewBinder;
 	}
 	
